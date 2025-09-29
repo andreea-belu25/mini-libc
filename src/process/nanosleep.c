c7dad2e25e4273af0ba0c1f96cbe83664d578f82
Andreea-Daniela Belu <119062819+andreea-belu25@users.noreply.github.com>
2025-09-29 15:32:03 +0300

    Update nanosleep.c

--- src/process/nanosleep.c
+++ src/process/nanosleep.c
@@ -6,17 +6,17 @@
 
 int nanosleep(const struct timespec *t1, struct timespec *t2)
 {
-    // 	nanosleep face syscall-ul si sleep cheama nanosleep-ul
-
-	// t1 si t2 imi ofera timpul pentru sleep al procesului
-	/* apel de sistem nanosleep; daca apelul a reusit 
-	ii returnez valoarea, altfel modific errno si returnez
-	-1 */
-    int rez = syscall(__NR_nanosleep, t1, t2);
-
-    if (rez >= 0)
-        return rez;
-
-    errno = -rez;
-    return -1;
+	// nanosleep makes the syscall and sleep calls nanosleep
+	// t1 and t2 provide the time for the process's sleep
+	
+	/* nanosleep system call; if the call succeeded
+	   I return its value, otherwise I modify errno and return -1 */
+	int rez = syscall(__NR_nanosleep, t1, t2);
+	
+	if (rez >= 0)
+		return rez;
+	
+	errno = -rez;
+	
+	return -1;
 }
