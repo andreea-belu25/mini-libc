daa272de2cdf50a33a29d266bb1a747e0899ca76
Andreea-Daniela Belu <119062819+andreea-belu25@users.noreply.github.com>
2025-09-29 15:31:15 +0300

    Update mmap.c

--- src/mm/mmap.c
+++ src/mm/mmap.c
@@ -6,59 +6,54 @@
 
 void *mmap(void *addr, size_t length, int prot, int flags, int fd, off_t offset)
 {
-	/* TODO: Implement mmap(). */
-
-	//  mmap -> rezerva o zona de memorie
-
-	/* addr = adresa de start 
-	   length = nr de bytes care trebuie mapati
-	   prot = tipul accesului permis
+	// mmap -> reserves a memory region
+	/* addr = start address 
+	   length = number of bytes to be mapped
+	   prot = type of access permitted
 	   flags = "nature of the map"
 	   fd = file descriptor
-	   offset = unde incepe fisierul care trebuie mapat */
-
-	//  syscall-ul se executa asemanator ca la restul
-
+	   offset = where the file to be mapped starts */
+	
+	// The syscall executes similarly to the rest
 	long rez = syscall(__NR_mmap, addr, length, prot, flags, fd, offset);
-
+	
 	if (rez >= 0)
 		return (void *)rez;
+	
 	errno = -rez;
 	return MAP_FAILED;
 }
 
 void *mremap(void *old_address, size_t old_size, size_t new_size, int flags)
 {
-	/* TODO: Implement mremap(). */
-
-	//  mremap -> maparea unei adrese virtuale de memorie
-
-	/*  old_address = adresa veche a zonei de memorie de extins
-		old_size = vechea marime a memoriei virtuale
-		new_size = noua marime dupa extindere */
-	//  syscall-ul se face asemanator ca pana acum
-
+	// mremap -> mapping of a virtual memory address
+	/*  old_address = old address of the memory region to expand
+		old_size = old size of virtual memory
+		new_size = new size after expansion */
+	
+	// The syscall is done similarly as before
 	long rez = syscall(__NR_mremap, old_address, old_size, new_size, flags);
-
+	
 	if (rez >= 0)
 		return (void*)rez;
+	
 	errno = -rez;
 	return MAP_FAILED;
 }
 
 int munmap(void *addr, size_t length)
 {
-	/* TODO: Implement munmap(). */
-	//  munmap -> stergerea unei zone de memorie mapata anterior
-
-	/*  addr = adresa de start a zonei mapate
-		length = lungimea zonei mapate */
-
-	//  syscall asemanator ca pana acum
+	// munmap -> deletion of a previously mapped memory region
+	/*  addr = start address of the mapped region
+		length = length of the mapped region */
+	
+	// Syscall similar as before
 	long rez = syscall(__NR_munmap, addr, length);
-
+	
 	if (rez >= 0)
 		return rez;
+	
 	errno = -rez;
+	
 	return -1;
 }
