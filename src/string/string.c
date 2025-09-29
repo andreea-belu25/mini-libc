f646761baf72c4302c962c452dce096ed0c41b34
Andreea-Daniela Belu <119062819+andreea-belu25@users.noreply.github.com>
2025-09-29 15:37:24 +0300

    Update string.c

--- src/string/string.c
+++ src/string/string.c
@@ -4,13 +4,12 @@
 
 char *strcpy(char *destination, const char *source)
 {
-	/* TODO: Implement strcpy(). */
-
-	//  copiez sirul sursa in sirul destinatie
+	// Copy the source string to the destination string
 	int index = 0;
 
 	for (index = 0; *(source + index); index++)
 		*(destination + index) = *(source + index);
+	
 	*(destination + index) = '\0';
 
 	return destination;
@@ -18,9 +17,7 @@ char *strcpy(char *destination, const char *source)
 
 char *strncpy(char *destination, const char *source, size_t len)
 {
-	/* TODO: Implement strncpy(). */
-
-	//  copiez len caracterere din sirul sursa in sirul destinatie
+	// Copy len characters from the source string to the destination string
 	int index = 0;
 
 	for (index = 0; index < (int)len; index++)
@@ -31,16 +28,16 @@ char *strncpy(char *destination, const char *source, size_t len)
 
 char *strcat(char *destination, const char *source)
 {
-	/* TODO: Implement strcat(). */
-
-	// determin lungimea sirului destinatie
-    int lung_string = 0, index = 0;
+	// Determine the length of the destination string
+	int lung_string = 0, index = 0;
+	
 	for (index = 0; *(destination + index); index++)
 		lung_string++;
 
-	// concatenez sirul sursa la sirul destinatie
+	// Concatenate the source string to the destination string
 	for (index = 0; *(source + index); index++)
 		*(destination + lung_string + index) = *(source + index);
+	
 	*(destination + lung_string + index) = '\0';
 
 	return destination;
@@ -48,14 +45,16 @@ char *strcat(char *destination, const char *source)
 
 char *strncat(char *destination, const char *source, size_t len)
 {
-	/* TODO: Implement strncat(). */
-	// determin lungimea sirului destinatie
+	// Determine the length of the destination string
 	int lung_string = 0, index = 0;
+	
 	for (index = 0; *(destination + index); index++)
 		lung_string++;
-	// concatenez len caractere din sirul sursa la sirul destinatie
+	
+	// Concatenate len characters from the source string to the destination string
 	for (index = 0; index < (int)len; index++)
 		*(destination + lung_string + index) = *(source + index);
+	
 	*(destination + lung_string + index) = '\0';
 
 	return destination;
@@ -63,47 +62,51 @@ char *strncat(char *destination, const char *source, size_t len)
 
 int strcmp(const char *str1, const char *str2)
 {
-	/* TODO: Implement strcmp(). */
-
 	int lung_str1 = 0, index = 0;
 
-	//  determin lungimea primului sir
+	// Determine the length of the first string
 	for (index = 0; *(str1 + index); index++)
 		lung_str1++;
-	//  determin lungimea sirului 2
+	
+	// Determine the length of string 2
 	int lung_str2 = 0;
+	
 	for (index = 0; *(str2 + index); index++)
 		lung_str2++;
-	/* lung maxima dintre siruri pentru parcurgerea 
-	completa a ambelor siruri */
+	
+	/* Maximum length between strings for complete traversal 
+	   of both strings */
 	int lung_max = 0;
+	
 	if (lung_str1 > lung_str2)
 		lung_max = lung_str1;
 	else
 		lung_max = lung_str2;
-	//  compar cele doua siruri dpdv lexicografic
+	
+	// Compare the two strings lexicographically
 	for (index = 0; index < lung_max ; index++) {
 		if (*(str1 + index) > *(str2 + index))
 			return 1;
 		if (*(str1 + index) < *(str2 + index))
 			return -1;
 	}
+	
 	return 0;
 }
 
 int strncmp(const char *str1, const char *str2, size_t len)
 {
-	/* TODO: Implement strncmp(). */
-
-	/* compar cele doua siruri dpdv lexicografic cat timp 
-	nu am parcurs len caractere */  
+	/* Compare the two strings lexicographically while 
+	   I haven't traversed len characters */
 	int index = 0;
-		for (index = 0; index < (int)len ; index++) {
+	
+	for (index = 0; index < (int)len ; index++) {
 		if (*(str1 + index) > *(str2 + index))
 			return 1;
 		if (*(str1 + index) < *(str2 + index))
 			return -1;
 	}
+	
 	return 0;
 }
 
@@ -119,94 +122,104 @@ size_t strlen(const char *str)
 
 char *strchr(const char *str, int c)
 {
-	/* TODO: Implement strchr(). */
-
-	/* caut caracterul c in sirul str si returnez adresa
-	unde am gasit caracterul in sir */
+	/* Search for character c in string str and return the address
+	   where I found the character in the string */
 	int index = 0;
-	/* cast pentru ca primeam warning daca adunam 
-	la ceva constant un intreg */
+	
+	/* Cast because I was getting a warning if adding 
+	   an integer to something constant */
 	char *source = (char *)str;
+	
 	for (index = 0; *(source + index); index++) {
 		if (*(source + index) == (char)c)
 			return source + index;
 	}
+	
 	return NULL;
 }
 
 char *strrchr(const char *str, int c)
 {
-	/* TODO: Implement strrchr(). */
-	// ultima pozitie a caracterului c in string (daca exista)
-
+	// Last position of character c in string (if it exists)
 	int index = 0, lung_string = 0;
 	char *string = (char*) str;
+	
 	for (index = 0; *(string + index); index++)
 		lung_string++;
-	//  parcurg sirul in sens invers
+	
+	// Traverse the string in reverse
 	for (index = lung_string - 1; index >= 0; index--)
 		if (*(string + index) == (char)c)
 			return string + index;
+	
 	return NULL;
 }
 
 char *strstr(const char *haystack, const char *needle)
 {
-	/* TODO: Implement strstr(). */
-
-	//  contor in haystack
+	// Counter in haystack
 	int index_0 = 0;
-	//  contor in needle
+	
+	// Counter in needle
 	int index_1 = 0;
-	//  cate caratere consec din needle se gasesc in haystack
+	
+	// How many consecutive characters from needle are found in haystack
 	int equal_characters = 0;
-	//  cast pentru a putea intoarce o expresie aritmetica
+	
+	// Cast to be able to return an arithmetic expression
 	char *needle_found = NULL, *h_stack = (char*)haystack, *ned = (char *)needle;
 
 	while (*(h_stack + index_0) && *(ned + index_1)) {
 		if (*(ned + index_1) == *(h_stack + index_0)) {
 			index_0++;
 			index_1++;
-			equal_characters++;  //  nr de caractere egale consecutive
-			//  adresa de inceput a sirului needle in sirul haystack
+			equal_characters++;  // Number of consecutive equal characters
+			
+			// Start address of the needle string in the haystack string
 			needle_found = h_stack + index_0;
 		} else {
 			index_0++;
-			//  reinitializez pozitia in needle si nr de caractere egale
+			
+			// Reinitialize the position in needle and the number of equal characters
 			index_1 = 0;
 			equal_characters = 0;
 		}
 	}
 
 	int lung_needle = 0;
+	
 	for (index_1 = 0; *(ned + index_1); index_1++)
 		lung_needle++;
-	/*  daca am intalnit tot sirul needle in sirul haystack 
-	=> returnez adresa, altfel NULL  */
+	
+	/* If I encountered the entire needle string in the haystack string 
+	   => return the address, otherwise NULL */
 	if (equal_characters == lung_needle)
 		return needle_found - equal_characters;
+	
 	return NULL;
 }
 
 char *strrstr(const char *haystack, const char *needle)
 {
-	/* TODO: Implement strrstr(). */
-	/* implementare asemanatoare cu strstr, 
-	dar parcurg sirul in sens invers */
-
+	/* Implementation similar to strstr, 
+	   but traverse the string in reverse */
 	int lung_needle = 0;
-	int index_1 = 0;  //  contor in needle
+	int index_1 = 0;  // Counter in needle
+	
 	for (index_1 = 0; *(needle + index_1); index_1++)
 		lung_needle++;
+	
 	int lung_haystack = 0;
-	int index_0 = 0;  //  contor in haystack
+	int index_0 = 0;  // Counter in haystack
+	
 	for(index_0 = 0; *(haystack + index_0); index_0++)
 		lung_haystack++;
+	
 	int equal_characters = 0;
 	char *needle_found = NULL, *h_stack = (char *)haystack, *ned = (char *)needle;
+	
 	index_0 = lung_haystack - 1;
 	index_1 = lung_needle - 1;
-
 
 	while (index_0 >= 0 && index_1 >= 0) {
 		if (*(ned + index_1) == *(h_stack + index_0)) {
@@ -220,21 +233,23 @@ char *strrstr(const char *haystack, const char *needle)
 			equal_characters = 0;
 		}
 	}
+	
 	if (equal_characters == lung_needle)
 		return needle_found;
+	
 	return NULL;
 }
 
 void *memcpy(void *destination, const void *source, size_t num)
 {
-	/* TODO: Implement memcpy(). */
-	/* copiez num biti din blocul de mem al lui source in cel al lui destination
-      fara buffer auxiliar */
-
-	//  acelasi motiv pentru castare
+	/* Copy num bytes from the memory block of source to that of destination
+	   without auxiliary buffer */
+	
+	// Same reason for casting
 	char *dst = (char*) destination, *src = (char*) source;
 
 	int index = 0;
+	
 	while (index < (int)num) {
 		*(dst + index) = *(src + index);
 		index++;
@@ -245,19 +260,20 @@ void *memcpy(void *destination, const void *source, size_t num)
 
 void *memmove(void *destination, const void *source, size_t num)
 {
-	/* TODO: Implement memmove(). */
-	//  copiez num biti din source in destination cu un buffer auxiliar
-
+	// Copy num bytes from source to destination with an auxiliary buffer
 	char *dst = (char *) destination, *src = (char*) source;
 	const int kNum = (int)num;
 	char aux[kNum];
+	
 	int index = 0;
+	
 	while (index < (int)num) {
 		*(aux + index) = *(src + index);
 		index++;
 	}
 
 	index = 0;
+	
 	while(index < (int)num) {
 		*(dst + index) = *(aux + index);
 		index++;
@@ -268,29 +284,29 @@ void *memmove(void *destination, const void *source, size_t num)
 
 int memcmp(const void *ptr1, const void *ptr2, size_t num)
 {
-	/* TODO: Implement memcmp(). */
-	/* < 0 => primul byte are o val mai mica in ptr1 daca in ptr2
-	   > 0 => primul byte are o val mai mare in ptr1 daca in ptr2
-	   = 0 => toti bitii sunt egali in ambele siruri*/
-
+	/* < 0 => the first byte has a smaller value in ptr1 than in ptr2
+	   > 0 => the first byte has a larger value in ptr1 than in ptr2
+	   = 0 => all bytes are equal in both strings */
+	
 	char *string1 = (char*)ptr1, *string2 = (char*)ptr2;
-	/* parcurg byte cu byte si returnez o valoare in fct de 
-	cazurile de mai sus */
+	
+	/* Traverse byte by byte and return a value according to 
+	   the cases above */
 	int index = 0;
+	
 	for (index = 0; index < (int)num; index++) {
 		if (*(string1 + index) > *(string2 + index))
 			return 1;
 		if (*(string1 + index) < *(string2 + index))
 			return -1;
 	}
+	
 	return 0;
 }
 
 void *memset(void *source, int value, size_t num)
 {
-	/* TODO: Implement memset(). */
-	// setez primii num biti din source cu value
-
+	// Set the first num bytes from source with value
 	char *src = (char *)source;
 	int index = 0;
 
