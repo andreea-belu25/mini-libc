3e2d28b802e1b63dde269993e9a877ce586275d9
Andreea-Daniela Belu <119062819+andreea-belu25@users.noreply.github.com>
2025-09-29 15:32:38 +0300

    Update sleep.c

--- src/process/sleep.c
+++ src/process/sleep.c
@@ -5,16 +5,19 @@
 
 unsigned int sleep(unsigned int seconds)
 {
-    //  declarare structuri t1 si t2 pentru apel la nanosleep
-    struct timespec t1 = {seconds, 0};
-    struct timespec t2 = {seconds, 0};
-	// seconds = timpul de asteptare (sleep) al procesului
-	/* apel de sistem sleep; daca apelul a reusit 
-	ii returnez valoarea, altfel modific errno si returnez
-	-1 */
-    int rez = nanosleep(&t1, &t2);
-    if (rez >= 0)
-        return rez;
-    errno = -rez;
-    return -1;
+	// Declare structures t1 and t2 for nanosleep call
+	struct timespec t1 = {seconds, 0};
+	struct timespec t2 = {seconds, 0};
+	
+	// seconds = the waiting time (sleep) of the process
+	
+	/* sleep system call; if the call succeeded
+	   I return its value, otherwise I modify errno and return -1 */
+	int rez = nanosleep(&t1, &t2);
+	
+	if (rez >= 0)
+		return rez;
+	
+	errno = -rez;
+	return -1;
 }
